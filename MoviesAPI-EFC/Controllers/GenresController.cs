431a8fc3968cbb37e2a19d2c6e9335ac072261a3
EmanuelTejada032 <84829473+EmanuelTejada032GameDev@users.noreply.github.com>
2023-12-13 15:07:39 -0400

    First commit - ef core project set up with genre controller basic get

---
+++ MoviesAPI-EFC/Controllers/GenresController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MoviesAPI_EFC.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GenresController: ControllerBase
+    {
+        private readonly ApplicationDbContext _moviesDbContext;
+
+        public GenresController(ApplicationDbContext moviesDbContext)
+        {
+            _moviesDbContext = moviesDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _moviesDbContext.Genres.ToListAsync()); 
+        }
+    }
+}
