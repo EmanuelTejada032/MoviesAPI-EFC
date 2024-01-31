86dfc9c1986eda02afd8306a317bbca1133635b0
EmanuelTejada032 <emanueltejada31295@gmail.com>
2024-01-30 23:19:45 -0400

    TypeBinder to bind complex properties in a from form attribute endpoint MoviesActors and MoviesGenres relationship a Insert genres on MoviesGenres table when creating movie insert actors on MoviesActors when creating movies
    update movies genres and movies actor on movies update

---
+++ MoviesAPI-EFC/Helpers/TypeBinder.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Newtonsoft.Json;
+
+namespace MoviesAPI_EFC.Helpers
+{
+    public class TypeBinder<T> : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            var propertyName = bindingContext.ModelName;
+            var valuesProvider = bindingContext.ValueProvider.GetValue(propertyName);
+
+            if(valuesProvider == ValueProviderResult.None) return Task.CompletedTask;
+
+            try
+            {
+                var deserializedValue = JsonConvert.DeserializeObject<T>(valuesProvider.FirstValue);
+                bindingContext.Result = ModelBindingResult.Success(deserializedValue);
+            }
+            catch
+            {
+                bindingContext.ModelState.TryAddModelError(propertyName, "Not valid List<int> value");
+            }
+
+            return Task.CompletedTask;
+
+        }
+    }
+}
